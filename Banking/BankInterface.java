59c97b75d5db13a95d3fbb5eb657c047f9290b41
Sakshi Singh <sakshisingh2882@gmail.com>
2024-07-09 17:32:53 +0530

    Create BankInterface.java

---
+++ Banking/BankInterface.java
@@ -0,0 +1,17 @@
+package bank;
+
+public interface BankInterface {
+    
+    /**
+     * Method to create and show the user interface (UI) for the bank operations.
+     */
+    void createAndShowUI();
+
+    /**
+     * Example of additional method:
+     * Method to update the UI when a transaction occurs.
+     */
+    void updateUIOnTransaction();
+
+    // Add more methods as needed for bank operations
+}
